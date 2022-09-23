9e411fc8789e2a947e968f9cbe6f1e02c79dea6d
Git-Gagan02 <107171517+Git-Gagan02@users.noreply.github.com>
2022-09-23 17:55:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   region = "us-east-1"
 
-  access_key = "AKIA3TC7FKYIXDCNKR3Y"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 
 }
