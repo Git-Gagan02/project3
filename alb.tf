c105c673dd320a93c1d14f0b283fbf3e07f07bea
root <root@ip-172-31-83-248.ec2.internal>
2022-09-26 12:06:19 +0000

    updated

--- alb.tf
+++ alb.tf
@@ -1,49 +1,50 @@
 #Creating External LoadBalancer
 
-resource "aws_lb" "external-alb" {
-    name = "External LB"
-    internal =false
-    load_balancer_type = "application"
-    security_groups = [aws_security_group.demosg.id]
-    subnets = [aws_subnet.public-1a.id, aws_subnet.public-subnet-1b.id]
+resource "aws_lb" "external-elb" {
+  name               = "External-LB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.demosg.id]
+  subnets = [aws_subnet.public-subnet-1a.id,
+  aws_subnet.public-subnet-1b.id]
 
 }
 
-resource "aws_lb_target_group" "target-elb" {
-    name = "ALB TG"
-    port = 80
-    protocol = "HTTP"
-    vpc_id = aws_vpc.demovpc.id
-  
+resource "aws_lb_target_group" "external-elb" {
+  name     = "alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.demovpc.id
+
 }
 
 resource "aws_lb_target_group_attachment" "attachment" {
-    target_group_arn = aws_lb_target_group.external-alb.arn
-    target_id = aws_instance.demoinstance.id
-    port = 80
-    depends_on = [
-      aws_instance.demoinstance
-    ]
-  
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id        = aws_instance.demoinstance.id
+  port             = 80
+  depends_on = [
+    aws_instance.demoinstance
+  ]
+
 }
 
-resource "aws_lb_target_group_attachement" "attachment" {
-    target_group_arn = aws_lb_target_group.external-alb.arn
-    target_id = aws_instance.demoinstance2.id
-    port = 80
-    depends_on = [
-      aws_instance.demoinstance2
-    ]
-  
+resource "aws_lb_target_group_attachment" "attachment1" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id        = aws_instance.demoinstance2.id
+  port             = 80
+  depends_on = [
+    aws_instance.demoinstance2
+  ]
+
 }
 
 resource "aws_lb_listener" "external-elb" {
-    load_balancer_arn = aws_lb.external-alb.arn
-    port = "80"
-    protocol = "HTTP"
-    default_action {
-      type ="forward"
-      target_group_arn = aws_lb_target_group.external-alb.arn
-    }
-  
+  load_balancer_arn = aws_lb.external-elb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.external-elb.arn
+  }
+
 }
